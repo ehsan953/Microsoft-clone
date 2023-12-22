f282a8b572b629332db00a47e56743e1856c50f1
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2023-12-22 18:53:33 +0500

    Add Responsive footer and toogle dropdown, All microsoft dropdown

---
+++ script.js
@@ -0,0 +1,16 @@
+// (function () {
+//   var toggle = document.querySelector(".nav-toggle");
+
+//   toggle.addEventListener("click", function (e) {
+//     this.classList.toggle("features");
+//   });
+// })();
+
+// function toggleClass() {
+//   var divElement = document.getElementById("nav-toggle");
+//   if (divElement.classList.contains("features")) {
+//     divElement.classList.remove("features");
+//   } else {
+//     divElement.classList.add("features");
+//   }
+// }
