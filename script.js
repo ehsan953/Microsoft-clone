85b85053e3a3510415f51e4957aa35eebc7d6e51
Muhammad Ehsan <muhammadehsanhassan781@gmail.com>
2023-12-26 15:40:04 +0500

    Add responsive dropdowns, toggle menu with javascript

--- script.js
+++ script.js
@@ -6,11 +6,34 @@
 //   });
 // })();
 
-// function toggleClass() {
-//   var divElement = document.getElementById("nav-toggle");
-//   if (divElement.classList.contains("features")) {
-//     divElement.classList.remove("features");
-//   } else {
-//     divElement.classList.add("features");
-//   }
-// }
+function toggleClass() {
+  var divElement = document.getElementById("menuBar");
+  if (divElement.classList.contains("left-[-100%]")) {
+    divElement.classList.remove("left-[-100%]");
+  } else {
+    divElement.classList.add("left-[-100%]");
+  }
+}
+function dropdownClass() {
+  var divElement = document.getElementById("dropdown");
+  if (divElement.classList.contains("hidden")) {
+    divElement.classList.remove("hidden");
+  } else {
+    divElement.classList.add("hidden");
+  }
+}
+function moreClass() {
+  var divElement = document.getElementById("more");
+  if (divElement.classList.contains("hidden")) {
+    divElement.classList.remove("hidden");
+  } else {
+    divElement.classList.add("hidden");
+  }
+}
+function blackClass() {
+  var divElement = document.getElementById("header");
+  if (divElement.classList.contains("bg-[#6B0E34]")) {
+    divElement.classList.remove("bg-[#6B0E34]");
+    divElement.classList.add("bg-black");
+  }
+}
